102eb4a186215e94828abbe43e5b8279cdb5e309
Nick Poole <NickPooleA@gmail.com>
2018-10-22 13:54:52 -0700

    Basic LibGDX starter project

---
+++ desktop/src/com/nickpoole/defendtheworld/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nickpoole.defendtheworld.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nickpoole.defendtheworld.DefendTheWorld;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DefendTheWorld(), config);
+	}
+}
